30057ca06e4e362335e483da7737940168627a34
Muhammad Kori <muhammad.kori@gmail.com>
2017-05-22 14:23:22 +0100

    creative tim

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var path = require('path');
+
+module.exports = {
+    entry: './app/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        loaders: [
+            {
+                test:   /\.css$/,
+                loader: "style-loader!css-loader!postcss-loader"
+            }
+        ]
+    }
+}
